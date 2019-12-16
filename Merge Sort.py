5a067fe485b08e22a13901058da259210d4046b6
levinh611 <56534873+levinh611@users.noreply.github.com>
2019-12-16 11:22:02 +0700

    Create Merge Sort.py

---
+++ Merge Sort.py
@@ -0,0 +1,30 @@
+def ms(A):
+    H = len(A)
+    if H > 1:
+        middle = (H) // 2
+        trai = A[:middle]
+        phai = A[middle:]
+        ms(trai)
+        ms(phai)
+        a = 0
+        b = 0
+        c = 0
+        while a < len(trai) and b < len(phai):
+           if trai[a] < phai[b]:
+             A[c] = trai[a]
+             a +=1
+           else:
+              A[c]= phai[b]
+              b +=1
+           c +=1
+        while a < len(trai):
+           A[c] = trai[a]
+           a +=1
+           c +=1
+        while b < len(phai):
+           A[c] = phai[b]
+           b +=1
+           c +=1
+A = [34, 56, 78,3]
+ms(A)
+print(A)
